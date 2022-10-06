8a63a1477f599ded6ef99f17228705b657af75bb
Arshia Nazarlotfi <arshia_nazarlotfi1376@yahoo.com>
2022-10-06 14:46:47 +0330

    header change

--- components/Common/Header.js
+++ components/Common/Header.js
@@ -10,6 +10,9 @@ const Header = (props) => {
       />
       <title>{props.title}</title>
       <meta name='description' content={props.description} />
+      <meta property='og:title' content={props.title} />
+      <meta property='og:description' content={props.description} />
+      <meta name='keywords' content='OnlineStore,Eccomerce,Nextjs' />
     </Head>
   );
 };
